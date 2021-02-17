9d5535611f64e6f50b1dc7555b5e4e25ce5496d8
Paul Browne <paulbrowne2@gmail.com>
2021-02-17 12:05:10 +0000

    Added toasts

--- bag/contexts.py
+++ bag/contexts.py
@@ -23,11 +23,11 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
+                total += int(quantity) * int(product.price)
+                product_count += int(quantity)
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
